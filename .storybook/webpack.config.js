110751b5fd3a47df8cf2f7db0d0c8397f9041f95
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-03-31 11:36:56 +0000

    Added storybook, stylelint, scss config, etc.

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,26 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add addional webpack configurations.
+// For more information refer the docs: https://getstorybook.io/docs/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
+  module: {
+    loaders: [
+      // add your custom loaders.
+      {
+        test: /.(css|scss)$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules=1&localIdentName=[hash:base64:16]',
+          'sass-loader?outputStyle=expanded',
+        ],
+      },
+    ],
+  },
+};
